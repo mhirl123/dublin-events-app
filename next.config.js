8b3dfdc88b02845dc5b98f3dc35262562aa7af2c
Mo Harvey <momoharvey@gmail.com>
2026-08-28 16:32:28 +0100

    Configure Next.js for Railway deployment with standalone output

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['ticketmaster.com', 'ticketmaster.ie', 'eventbrite.com'],
   },
